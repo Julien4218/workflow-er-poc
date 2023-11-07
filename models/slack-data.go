b1d99d4aa9902160e95d90406daee055ec672ec9
jwilkinson <jwilkinson@newrelic.com>
2023-11-07 12:53:39 -0700

    chore: clean up no longer needed files

--- models/slack-data.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "github.com/Julien4218/temporal-slack-activity/models"
-
-type SlackData struct {
-	ChannelId            string
-	FirstResponseWarning string
-	Attachment           models.MessageAttachment
-}
