4d3f343f877e67a70476c38b8ea7a3134c8a3ff0
jwilkinson <jwilkinson@newrelic.com>
2023-11-09 09:49:36 -0700

    feat: added child workflows to the ER Workflow

---
+++ workflows/retrospective-workflow.go
@@ -0,0 +1,45 @@
+package workflows
+
+import (
+	"errors"
+	slackActivities "github.com/Julien4218/temporal-slack-activity/activities"
+	slackModels "github.com/Julien4218/temporal-slack-activity/models"
+	"github.com/Julien4218/workflow-poc/instrumentation"
+	"github.com/sirupsen/logrus"
+	"go.temporal.io/sdk/workflow"
+	"os"
+	"time"
+)
+
+const RetrospectiveWorkflowName = "RetrospectiveWorkflow"
+
+type RetrospectiveWorkflowInput struct {
+}
+
+func RetrospectiveWorkflow(ctx workflow.Context, input *RetrospectiveWorkflowInput) (string, error) {
+	logrus.Infof("%s-SlackWorkflow started:%s", instrumentation.Hostname, RetrospectiveWorkflowName)
+	defer logrus.Infof("%s-SlackWorkflow completed:%s", instrumentation.Hostname, RetrospectiveWorkflowName)
+	txn := instrumentation.NrApp.StartTransaction(RetrospectiveWorkflowName)
+	defer txn.End()
+
+	ctx = updateIncidentWorkflowContextOptions(ctx, 10*time.Minute)
+	logrus.Infof("Got retrospective input:%s", input)
+
+	slackChannel := os.Getenv("SLACK_CHANNEL")
+	if slackChannel == "" {
+		return "", errors.New("required environment variable SLACK_CHANNEL is not set")
+	}
+	message := "Starting retrospective"
+	messageData := slackModels.SlackActivityData{
+		ChannelId:            slackChannel,
+		FirstResponseWarning: message,
+	}
+	var result slackModels.MessageDetails
+	if err := workflow.ExecuteActivity(ctx, slackActivities.PostMessageActivity, messageData).Get(ctx, &result); err != nil {
+		logrus.Errorf("Activity failed. Error: %s", err)
+		return "", err
+	}
+	logrus.Infof("%s workflow completed.", RetrospectiveWorkflowName)
+
+	return "DONE", nil
+}
